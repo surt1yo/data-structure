d56ce848e6c83c25abd4e0276b9af3fa900b9a8b
surt1yo <mezaan.rizv@gmail.com>
2025-11-20 22:09:12 +1300

    initial commit

--- afterclass.py
+++ afterclass.py
@@ -1,9 +1,9 @@
 #Create a list of square values of numbers between specified ranges by the user, 
 #and then separate the odd and even values.
-start = int(input("Enter start of range: "))
-end = int(input("Enter end of range: "))
-squares = [x ** 2 for x in range(start, end + 1)]
-even_squares = [sq for sq in squares if sq % 2 == 0]
-odd_squares = [sq for sq in squares if sq % 2 != 0]
+start=int(input("Enter start of range: "))
+end=int(input("Enter end of range: "))
+square=[x ** 2 for x in range(start, end + 1)]
+even_squares=[sq for sq in square if sq % 2 == 0]
+odd_squares=[sq for sq in square if sq % 2 != 0]
 print("Even squares:", even_squares)
 print("Odd squares:", odd_squares)
